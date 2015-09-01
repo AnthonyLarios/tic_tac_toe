d01928c004744404d2dabc9d6e0a71bddc1284e5
Anthony Larios <simpsonsruleA52@gmail.com>
2015-08-31 21:45:32 -0700

    Modify get_cell method in board.rb to be public

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -5,14 +5,15 @@ module TicTacToe
       @grid = input.fetch(:grid, default_grid)
     end
 
+    def get_cell(x, y)
+      grid[y][x]
+    end
+
     private
 
     def default_grid
       Array.new(3) { Array.new(3) {Cell.new} }
     end
 
-    def get_cell(x, y)
-      grid[y][x]
-    end
   end
 end
